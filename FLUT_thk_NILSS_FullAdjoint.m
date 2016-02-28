9c73eb904e5b9cee9481ab41aa031101cda363e0
naxACDL <niangxiu@mit.edu>
2016-02-28 10:58:30 -0500

    now the optimization is performed on the adjoint of the entire field. use the  FLUT_thk_NILSS_FullAdjoint.m file. integration time for adjoint: from t=6 to t=16.
    Signed-off-by: naxACDL <niangxiu@mit.edu>

--- FLUT_thk_NILSS_FullAdjoint.m
+++ FLUT_thk_NILSS_FullAdjoint.m
@@ -1,50 +1,29 @@
 %%%%%%%%%%%% Jack Optimization %%%%%%%%%%
-function FLUT_thk()
+% this function performs minimization on the adjoint of the whole field
+function FLUT_thk_NILSS_FullAdjoint()
 sdir='thksens';
 resume = 0;
 if resume == 0
-    % prepare the primal solution
-    scomd=sprintf('zeus dir=%s main_init.inp',sdir); 
-    dos(scomd);
-    pause(0.5);
+    %% prepare the primal solution
+%     scomd=sprintf('zeus dir=%s main_init.inp',sdir); 
+%     dos(scomd);
+%     pause(0.5);
 
     %% prepare the non-homogeneous solution
     % main0.inp file, homo =0, requires no input files
     dos('copy main0_nonhomo.inp main0.inp /Y');
     % compute v*
-    vstar = run();
+    [vstar_obj, vstar_opt] = run(2);
 
-    % prepare the homogeneous solutions
-    nus = 60;
-    w = zeros(length(vstar), nus);
+    %% prepare the homogeneous solutions
+    nus = 2;
+    w_obj = zeros(length(vstar_obj), nus);
+    w_opt = zeros(length(vstar_opt), nus);
     for ius = 1: nus
         % main_0.inp file
         dos('copy main0_homo.inp main0.inp /Y');
-        % ADJSOL_IN.dat file
-        NMDS = 3;
-        NXT = 139;
-        NYT = 2;
-        NZT = 57;
-        fid = fopen('ADJSOL_IN.dat','wt');
-        for i = 1: 2
-            temp = rand(1, 2* NMDS);
-            fprintf(fid, '  %13.7e', temp);
-            fprintf(fid, '\n');
-        end
-        for ii = 1: 2
-        for i = 2: NXT
-            for j = 2: NYT
-                for k = 2: NZT
-                    temp = rand(1, 5);
-                    fprintf(fid,'  %13.7e', temp);
-                    fprintf(fid, '\n');            
-                end
-            end
-        end
-        end
-        fclose(fid);
         % copute homogeneous solutions: w
-        w(:,ius) = run();
+        [w_obj(:,ius), w_opt(:,ius)] = run(1);
     end
     % save data
     save('vstar_w');
@@ -56,22 +35,25 @@ elseif resume == 1
 end
 
 %% construct left matrix and right hand side, compute v
+disp('...construct left matrix and right hand side, compute v');
 M = zeros(nus, nus);
 for i = 1: nus
     for j = 1: nus
-    M(i,j) = dot( w(:,i) , w(:,j));
+    M(i,j) = dot( w_opt(:,i) , w_opt(:,j));
     end
 end
 rhs = zeros(nus,1);
 for i = 1:nus
-    rhs(i) = dot(vstar, w(:,i));
-end 
-v = vstar - ((M\rhs)' * w')';
+    rhs(i) = dot(vstar_opt, w_opt(:,i));
+end  
+v_opt = vstar_opt - ((M\rhs)' * w_opt')';
+v_obj = vstar_obj - ((M\rhs)' * w_obj')';
+v = v_obj;
 % save data
 save('v_vstar_w');
 
-
-%% compute derivative
+%% read in coefficients
+disp('read in coefficients')
 %READ IN FUM0 AND FLM0
 fid=fopen('ADJAFOIL2.DAT','r');   
 linn=fgets(fid);
@@ -99,12 +81,12 @@ end
 fclose(fid);
 
 % read in steps
-fid=fopen('ADJOUTP_UNST.DAT','r');  
+fid=fopen('ADJOUTP_UNST.DAT','r');
 linn=fgets(fid);
 xy=sscanf(linn,'%d %d %d %f');
 NSTEP=xy(1);
 NSTEP=NSTEP+1; %INCLUDING THE INITIAL STEADY ADJOINT
-LSTEP=1501;
+LSTEP=5001;
 fclose(fid)
 
 % read in FUM and FLM, first run main00.inp 
@@ -137,6 +119,8 @@ for n=1:nht            %THIS LOOP IS NOT ACTIVATED YET
 end
 fclose(fid);       
 
+%% compute derivative
+disp('compute derivative')
 % reshape VAL and VAU
 v = reshape(v,[],2);
 VAL = reshape(v(:, 1), nsnc(3),nsnc(2), LSTEP);
@@ -165,11 +149,10 @@ for istep=1:LSTEP
 end
 
 
-% fid=fopen('GRAD_GRID.DAT','r');          
-% linn=fgets(fid);
-% xy=sscanf(linn,'%f');
-% DV=xy(1);
-DV = 0.254000013E-03;
+fid=fopen('GRAD_GRID.DAT','r');          
+linn=fgets(fid);
+xy=sscanf(linn,'%f');
+DV=xy(1);
 
 % window function
 t = linspace(0,1,LSTEP);
@@ -183,7 +166,40 @@ end
 %     V = [0;0];
 % end
 
-function V = run()
+%% function run
+function [V_obj, V_opt] = run(homo)
+% homo = 0: non-homo with 0 terminal condition
+% homo = 1: homo
+V_opt = [];
+% need an empty output file
+dos('del ADJSOL_OUT.dat');            
+dos('copy ADJSOL_EMPTY.dat ADJSOL_OUT.dat /Y');
+
+% prepare random ADJSOL_IN.dat file
+fid = fopen('ADJSOL_IN.dat','wt');
+for i = 1: 2
+    temp = rand(1, 6);
+    if homo == 2
+        temp = zeros(1, 6);
+    end
+    V_opt = [V_opt; temp'];
+    fprintf(fid, '  %18.11e', temp);
+    fprintf(fid, '\n');
+end
+for ii = 1: 15456
+    temp = rand(1, 5);
+    if homo == 2
+        temp = zeros(1, 5);
+    end
+    V_opt = [V_opt; temp'];
+    fprintf(fid,'  %18.11e', temp);
+    fprintf(fid, '\n');            
+end
+fclose(fid);
+% if homo is 0 then the ADJSOL_IN is meaningless
+% if homo == 0
+%     V_opt = zeros(size(V_opt));
+% end
 
 %GET TARGET AIRFOIL Cp
 format long;
@@ -208,7 +224,7 @@ linn=fgets(fid);
 xy=sscanf(linn,'%d %d %d %f');
 NSTEP=xy(1);
 NSTEP=NSTEP+1; %INCLUDING THE INITIAL STEADY ADJOINT
-LSTEP = 1501;
+LSTEP = 5001;
 %LSTEP=xy(2);
 %LSTEP=LSTEP+1; %INCLUDING THE INITIAL STEADY ADJOINTLAEROL=xy(3);
 %DT=xy(4);
@@ -236,7 +252,15 @@ for ns=1:nsnc(2)
 end
 end
 fclose(fid);
+V_obj = [reshape(VAL,[],1); reshape(VAU,[],1)];
 
-V = [reshape(VAL,[],1); reshape(VAU,[],1)];
+%READ IN values for optimization: full adjoint solution at t = 0,end
+fid=fopen('ADJSOL_OUT.dat','r');  
+while ~feof(fid)
+    linn=fgets(fid);
+    xy=sscanf(linn,'%f');
+    V_opt = [V_opt; xy];
+end
+fclose(fid);
 
 end
